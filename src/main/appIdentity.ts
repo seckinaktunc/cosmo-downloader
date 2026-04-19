1430849eb0769433f8ea9fc8f7f023ceaf64b6bb
Seçkin Aktunç <95690007+seckinaktunc@users.noreply.github.com>
2026-04-19 03:59:13 +0300

    Update app identity

--- src/main/appIdentity.ts
+++ src/main/appIdentity.ts
@@ -1,5 +1,5 @@
 import appIcon from '../../build/icon.png?asset'
 
-export const APP_ID = 'com.cosmo.downloader'
+export const APP_ID = 'com.seckinaktunc.cosmodownloader'
 export const APP_NAME = 'Cosmo Downloader'
 export const APP_ICON = appIcon
